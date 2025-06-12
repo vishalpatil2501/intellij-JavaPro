c9b0848daad7261cf8986cf1ea9ac8f2b107f067
vishal.sonune <sonunevishal2501@gmail.com>
2025-06-12 12:44:47 +0530

    tp

---
+++ src/main/java/Abstraction/Abstarct_clasgs.java
@@ -0,0 +1,15 @@
+package Abstraction;
+
+interface Vehicle {
+    void start();
+    void stop();
+}
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car started");
+    }
+    public void stop() {
+        System.out.println("Car stopped");
+    }
+}
+
